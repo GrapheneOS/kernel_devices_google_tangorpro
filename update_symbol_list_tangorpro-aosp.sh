28104ae95f42ab9c4aed05236dc8a46415d538b2
Robin Peng <robinpeng@google.com>
2022-05-09 09:59:05 +0800

    update_symbol_list.sh: update tangorpro build files
    * update_symbol_list_tangorpro-aosp.sh for aosp kernel
    * update_symbol_list_tangorpro-staging.sh for staging kernel
    
    Bug: 230573746
    Signed-off-by: Robin Peng <robinpeng@google.com>
    Change-Id: I66d38b6e30aed4b9db14558b1854bc269108614b

--- update_symbol_list_tangorpro-aosp.sh
+++ update_symbol_list_tangorpro-aosp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
+BUILD_SCRIPT="./update_symbol_list_tangorpro.sh" \
 DEVICE_KERNEL_BUILD_CONFIG=private/devices/google/tangorpro/build.config.tangorpro \
 private/gs-google/update_symbol_list.sh "$@"
